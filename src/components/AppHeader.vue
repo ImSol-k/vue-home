9c1282aba367a94ccb850eec130b0505394beb97
yss9358 <93y2s@naver.com>
2024-04-15 18:38:05 +0900

    슬라이드 일단완성

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,79 @@
+<template>
+
+<header>
+    <div class="header0">
+        <div class="header1">
+            <!-- 누르면 둘다 홈으로 -->
+            <img src="../assets/images/homedeco/homelogo.png">
+            <span>
+                <a>집꾸며조</a>
+            </span>
+        </div>
+        <!-- //header1 -->
+
+        <div class="header2">
+            <!-- 검색 키워드 사용 -->
+            <!-- v-on:keyup.enter="submit" -->
+            <img src="../assets/images/homedeco/search.png">
+            <input type="text" name="keyword" value="" placeholder="제목으로 검색하세요.">
+        </div>
+        <!-- //header2 -->
+
+        <div class="header3">
+            <!-- 누르면 장바구니 페이지로 -->
+            <img src="../assets/images/homedeco/cart.png">
+
+            <!-- 로그인하기전 -->
+            <ul>
+                <li><a>로그인</a></li>
+                <li><a>회원가입</a></li>
+                <li><a>고객센터</a></li>
+            </ul>
+
+            <!-- 로그인하고나서 -->
+            <!-- <ul>   
+                <li><a>ㅁㅁㅁ님</a></li>
+                <li><a>로그아웃</a></li>
+                <li><a>마이페이지</a></li>
+            </ul> -->
+            <!-- 관리자가 로그인했을때 -->
+            <!-- <ul>   
+                <li><a>관리자</a></li>
+                <li><a>로그아웃</a></li>
+                <li><a>관리페이지</a></li>
+            </ul> -->
+        </div>
+        <!-- //header3 -->
+    </div>
+    <!-- header0 -->
+</header>
+<!-- //header 부분 -->
+
+</template>
+
+<script>
+import '@/assets/css/main/ss-home.css';
+
+export default {
+    name : 'AppHeader',
+    components : {
+
+    },
+    data (){
+        return {
+
+        }
+    },
+    methods : {
+
+    },
+    created (){
+
+    }
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SlideView.vue
@@ -0,0 +1,70 @@
+<template>
+	<div class="wrapper">
+		<Carousel :autoplay="5000" :wrap-around="true">
+			<Slide v-for="slide in slides" :key="slide">
+				<div class="carousel__item">
+					<img class="slideImg" :src="slide" />
+				</div>
+			</Slide>
+
+			<template #addons>
+				<Pagination />
+			</template>
+		</Carousel>
+	</div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { Carousel, Pagination, Slide } from "vue3-carousel";
+import home1 from "@/assets/images/main/mainslide/home1.avif";
+import home2 from "@/assets/images/main/mainslide/home2.avif";
+import home3 from "@/assets/images/main/mainslide/home3.avif";
+import home4 from "@/assets/images/main/mainslide/home4.avif";
+import home5 from "@/assets/images/main/mainslide/home5.avif";
+
+import "vue3-carousel/dist/carousel.css";
+
+export default defineComponent({
+	name: "SlideView",
+	components: {
+		Carousel,
+		Slide,
+		Pagination,
+	},
+	data() {
+		return {
+			slides: [home1, home2, home3, home4, home5]
+		};
+	},
+});
+</script>
+
+<style>
+.carousel__item {
+	color: var(--vc-clr-white);
+	font-size: 20px;
+	border-radius: 8px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: relative;
+	z-index: 0;
+}
+
+.carousel__slide {
+	padding: 10px;
+}
+
+.carousel__prev,
+.carousel__next {
+	box-sizing: content-box;
+	/* border: 5px solid white; */
+}
+.slideImg {
+	max-width: 100%;
+}
+.wrapper {
+	margin-top: 3vh;
+}
+</style>
