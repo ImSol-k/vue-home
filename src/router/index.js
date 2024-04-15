ef1b0767808d3afb8a46e1d597c5f9d0494f3075
ImSol-k <tiq0015@naver.com>
2024-04-15 16:53:45 +0900

    addRouter

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,59 @@
 import { createRouter, createWebHistory } from 'vue-router'
 //메인
+//승수
+import MainView from '../views/main_ss/MainView.vue'
+import CategoryView from '../views/main_ss/CategoryView.vue'
+//소리
 import ProductInfoView from '../views/product_sr/ProductInfoView.vue'
+import ProductCartView from '../views/product_sr/ProductCartView.vue'
+//지원
+import MyPageView from '../views/user_jo/MyPageView.vue'
+import LoginView from '../views/user_jo/LoginView.vue'
+import PayView from '../views/user_jo/PayView.vue'
+
 //매니저
+//예슬
 import productInsertView from '../views/manager_ys/productInsertView.vue'
 import productListView from '../views/manager_ys/productListView.vue'
+import BedListView from '../views/manager_ys/BedListView.vue'
+//재현
+import ManagerChart from '../views/manager_jh/ManagerChart.vue'
 
 const routes = [
   {
-    path: '/main/product',
+    path: '/',
+    name: 'main',
+    component: MainView
+  },
+  {
+    path: '/category',
+    name: 'category',
+    component: CategoryView
+  },
+  {
+    path: '/product',
     name: 'product',
     component: ProductInfoView
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: ProductCartView
+  },
+  {
+    path: '/mypage',
+    name: 'mypage',
+    component: MyPageView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/pay',
+    name: 'pay',
+    component: PayView
   },
   {
     path: '/manager/productinsert',
@@ -20,6 +64,11 @@ const routes = [
     path: '/manager/productlist',
     name: '/manager/productlist',
     component: productListView
+  },
+  {
+    path: '/manager/bedlist',
+    name: '/manager/bedlist',
+    component: BedListView
   }
 ]
 
