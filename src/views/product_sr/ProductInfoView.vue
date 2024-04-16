9c490260d56fadd0a5f93fb98a7ca04227456d7f
ImSol-k <tiq0015@naver.com>
2024-04-16 17:05:58 +0900

    token

--- src/views/product_sr/ProductInfoView.vue
+++ src/views/product_sr/ProductInfoView.vue
@@ -5,50 +5,53 @@
         <div class="infoContentA">
             <img src="@/assets/images/product/test.png" alt="">
 
-            <div class="productInfo">
-                <p>{{ productVo.category }}</p>
-                <h1 class="productName">{{ productVo.name }}</h1>
-                <div class="productStar">
-                    <a href="">
-                        <span v-for="(star, i) in 5" :key="i">
-                            <img v-if="starTotal > i" src="@/assets/images/homedeco/star.png" alt="">
-                            <img v-else src="@/assets/images/homedeco/star2.png" alt="">
-                        </span>
-                        <span>2개 리뷰</span>
-                    </a>
-                </div>
-                <div class="productPrice">
-                    <span>{{ showPrice }}</span>
-                    <span>원</span>
-                </div>
-                <div class="productContent">
-                    <select name="" id="" v-on:change="cartAdd">
-                        <option disabled selected>색상을 선택해주세요</option>
-                        <option v-for="(color, i) in productColor.length" :key="i" :value="productColor[i]">{{
-                    productColor[i] }}</option>
-                    </select>
-                    <div class="miniCart" v-for="(productCart, i) in productCarts" :key="i">
-                        <div class="cartproductName">
-                            <span>{{ productVo.name }}({{ productCarts[i].color }})</span>
-                            <span class="deleteCart">×</span>
-                        </div>
-                        <div class="countBtn">
-                            <button v-on:click.prevent="countClick(i, 'minus')">-</button>
-                            <span>{{ productCarts[i].count }}</span>
-                            <button v-on:click.prevent="countClick(i, 'plus')">+</button>
-                        </div>
-                    </div><!-- miniCart -->
-                    <div class="CartPrice">
-                        <span>주문금액</span>
-                        <span>{{ priceTotal }}</span>
+            <form v-on:submit.prevent="cartUpdate" action="">
+
+                <div class="productInfo">
+                    <p>{{ productVo.category }}</p>
+                    <h1 class="productName">{{ productVo.name }}</h1>
+                    <div class="productStar">
+                        <a href="">
+                            <span v-for="(star, i) in 5" :key="i">
+                                <img v-if="starTotal > i" src="@/assets/images/homedeco/star.png" alt="">
+                                <img v-else src="@/assets/images/homedeco/star2.png" alt="">
+                            </span>
+                            <span>2개 리뷰</span>
+                        </a>
+                    </div>
+                    <div class="productPrice">
+                        <span>{{ showPrice }}</span>
                         <span>원</span>
                     </div>
-                    <div class="infoCartPayBtn btnColor">
-                        <button type="button">장바구니</button>
-                        <button type="button">바로구매</button>
+                    <div class="productContent">
+                        <select name="" id="" v-on:change="cartAdd">
+                            <option disabled selected>색상을 선택해주세요</option>
+                            <option v-for="(color, i) in productColor.length" :key="i" :value="productColor[i]">
+                                {{ productColor[i] }}</option>
+                        </select>
+                        <div class="miniCart" v-for="(productCart, i) in productCarts" :key="i">
+                            <div class="cartproductName">
+                                <span>{{ productVo.name }}({{ productCarts[i].color }})</span>
+                                <span class="deleteCart" v-on:click="cartRemove(i)">×</span>
+                            </div>
+                            <div class="countBtn">
+                                <button v-on:click.prevent="countClick(i, 'minus')">-</button>
+                                <span>{{ productCarts[i].count }}</span>
+                                <button v-on:click.prevent="countClick(i, 'plus')">+</button>
+                            </div>
+                        </div><!-- miniCart -->
+                        <div class="CartPrice">
+                            <span>주문금액</span>
+                            <span>{{ showPriceTotal }}</span>
+                            <span>원</span>
+                        </div>
+                        <div class="infoCartPayBtn btnColor">
+                            <button type="submit">장바구니</button>
+                            <button type="submit">바로구매</button>
+                        </div>
                     </div>
-                </div>
-            </div><!-- productInfo -->
+                </div><!-- productInfo -->
+            </form>
         </div><!-- info-content-a -->
         <div class="infoCategory">
             <ul>
@@ -120,37 +123,40 @@
                 </div><!-- reviewContent -->
             </div>
 
-            <div class="infoSideCart">
-                <div class="sideCartSelect">
-                    <select name="" id="">
-                        <option disabled selected>색상을 선택해주세요</option>
-                        <option v-for="(color, i) in productColor.length" :key="i"
-                            v-on:click.prevent="cartAdd(productColor[i])">{{ productColor[i] }}</option>
-                    </select>
-                </div>
-
-                <div class="miniCart sideCart" v-for="(productCart, i) in productCarts" :key="i">
-                    <div class="cartproductName">
-                        <span>{{ productVo.name }}({{ productCarts[i].color }})</span>
-                        <span class="deleteCart">×</span>
+            <form v-on:submit.prevent="cartUpdate" action="">
+                <div class="infoSideCart">
+                    <div class="sideCartSelect">
+                        <select name="" id="" v-on:change="cartAdd">
+                            <option disabled selected>색상을 선택해주세요</option>
+                            <option v-for="(color, i) in productColor.length" :key="i" :value="productColor[i]">
+                                {{ productColor[i] }}</option>
+                        </select>
                     </div>
-                    <div class="countBtn">
-                        <button v-on:click.prevent="countClick('minus')">-</button>
-                        <span>{{ productVo.count }}</span>
-                        <button v-on:click.prevent="countClick('plus')">+</button>
-                    </div>
-                </div><!-- miniCart -->
 
-                <div class="sideCartPrice">
-                    <span>주문금액</span>
-                    <span>{{ priceTotal }}</span>
-                    <span>원</span>
+                    <div class="miniCart sideCart" v-for="(productCart, i) in productCarts" :key="i">
+                        <div class="cartproductName">
+                            <span>{{ productVo.name }}({{ productCarts[i].color }})</span>
+                            <span class="deleteCart">×</span>
+                        </div>
+                        <div class="countBtn">
+                            <button v-on:click.prevent="countClick('minus')">-</button>
+                            <span>{{ productCarts[i].count }}</span>
+                            <button v-on:click.prevent="countClick('plus')">+</button>
+                        </div>
+                    </div><!-- miniCart -->
+
+                    <div class="sideCartPrice">
+                        <span>주문금액</span>
+                        <span>{{ showPriceTotal }}</span>
+                        <span>원</span>
+                    </div>
+                    <div class="sideCartBtn btnColor">
+                        <button type="submit">장바구니</button>
+                        <button type="submit">바로구매</button>
+                    </div>
                 </div>
-                <div class="sideCartBtn btnColor">
-                    <button>장바구니</button>
-                    <button>바로구매</button>
-                </div>
-            </div>
+            </form>
+
         </div><!-- infoContent -->
 
         <AppFooter />
@@ -194,6 +200,7 @@ import '@/assets/css/product/cart.css'
 import '@/assets/css/product/infomain.css'
 import AppHeader from '@/components/AppHeader.vue'
 import AppFooter from '@/components/AppFooter.vue'
+import axios from 'axios';
 
 export default {
     name: "ProductInfoView",
@@ -214,8 +221,9 @@ export default {
             productColor: ["white", "black", "wood"],
             showPrice: "",
             priceTotal: 0,
+            showPriceTotal: 0,
             reviewVo: {
-                product : "",
+                product: "",
                 star: "",
                 content: "",
                 file: ""
@@ -223,34 +231,64 @@ export default {
         };
     },
     methods: {
+        //상품선택 *****************************************
         showProduct() {
             console.log("상품정보: " + this.productVo.name);
             this.showPrice = Number(this.productVo.price).toLocaleString('ko-KR');
         },
         cartAdd(event) {
             console.log("색상선택: " + event.target.value);
-            this.productVo.count = 1;
-            this.productVo.color = event.target.value;
 
-            this.productCarts.push(this.productVo);
-            console.log(this.productCarts.length);
-            for(let i = 0; i < this.productCarts.length; i++){
-                console.log(this.productCarts[i]);
+            let cart = {
+                name: this.productVo.name,
+                price: this.productVo.price,
+                count: 1,
+                category: this.productVo.category,
+                color: event.target.value
+
             }
+            let colorChack = this.productCarts.some(cart => cart.color === event.target.value);
+            if (!colorChack || this.productCarts.length == 0) {
+                this.priceTotal += this.productVo.price;
+                this.showPriceTotal = Number(this.priceTotal).toLocaleString('ko-KR');
 
-            //총금액 계산
-            this.priceTotal = this.productVo.price * this.productVo.count;
-            this.priceTotal = Number(this.priceTotal).toLocaleString('ko-KR');
+                this.productCarts.push(cart);
+            } else {
+                alert("추가된 상품입니다.");
+            }
         },
         countClick(num, type) {  //카운트
-            console.log(type + "클릭" + "/" + num);
+            console.log(type + " 클릭: " + num);
+
             if (type == "plus") {
                 this.productCarts[num].count++;
-            } else if (type == "minus" && this.productVo.count > 0) {
+                this.priceTotal += this.productVo.price;
+            } else if (type == "minus" && this.productCarts[num].count > 0) {
                 this.productCarts[num].count--;
+                this.priceTotal -= this.productVo.price;
             }
-            this.priceTotal = this.productVo.price * this.productVo.count;
-            this.priceTotal = Number(this.priceTotal).toLocaleString('ko-KR');
+            this.showPriceTotal = Number(this.priceTotal).toLocaleString('ko-KR');
+        },
+        cartRemove(i) {
+            console.log("카트삭제");
+            this.productCarts.splice(i, 1);
+        },
+        //장바구니 ******************************************
+        cartUpdate() {
+            console.log("장바구니 추가");
+            
+            axios({
+                method: '',
+                url: '', //SpringBoot주소
+                headers: { "Content-Type": "application/json; charset=utf-8" },
+                data: this.productCarts,
+                responseType: 'json'
+            }).then(response => {
+                console.log(response); //수신데이터
+
+            }).catch(error => {
+                console.log(error);
+            });
         },
         //리뷰관련 메소드 ********************************************
         reviewStar(star) {
