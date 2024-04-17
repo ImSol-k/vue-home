a23048dc749891763a4573c3c19f5398a374d4c4
aassdd236 <aassdd236@naver.com>
2024-04-17 18:42:19 +0900

    06:42

--- src/views/manager_ys/productListView.vue
+++ src/views/manager_ys/productListView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="wrap">
-        <AppHeader />
+        <AppHeaderManager />
 
         <div class="content">
             <div class="side">
@@ -11,7 +11,7 @@
                         <span v-else> ▽</span> <!-- 화살표 아이콘 -->
                     </li>
                     <ul v-show="showCategories"> <!-- 카테고리 리스트 -->
-                        <li class="cate-li"><strong>전체 상품 보기</strong></li>
+                        <li class="cate-li"><strong v-on:click.prevent="getList">전체 상품 보기</strong></li>
                         <li class="cate-li">
                             <ul>
                                 <li @click="toggleCategories00">
@@ -20,9 +20,9 @@
                                     <span v-else> ▽</span> <!-- 화살표 아이콘 -->
                                 </li>
                                 <ul v-show="showCategories00"> <!-- 카테고리 리스트 -->
-                                    <li class="cate-li"><a href="">침대</a></li>
-                                    <li class="cate-li"><a href="">매트리스</a></li>
-                                    <li class="cate-li"><a href="">침대 프레임</a></li>
+                                    <li class="cate-li" v-on:click.prevent="showProductList('bed')"><a href="">침대</a></li>
+                                    <li class="cate-li" v-on:click.prevent="showProductList('mattress')"><a href="">매트리스</a></li>
+                                    <li class="cate-li" v-on:click.prevent="showProductList('frame')"><a href="">침대 프레임</a></li>
                                 </ul>
                             </ul>
                         </li>
@@ -34,9 +34,9 @@
                                     <span v-else> ▽</span> <!-- 화살표 아이콘 -->
                                 </li>
                                 <ul v-show="showCategories02"> <!-- 카테고리 리스트 -->
-                                    <li class="cate-li"><a href="">일반 쇼파</a></li>
-                                    <li class="cate-li"><a href="">좌식 쇼파</a></li>
-                                    <li class="cate-li"><a href="">1인용 쇼파</a></li>
+                                    <li class="cate-li" v-on:click.prevent="showProductList('normal-sofa')"><a href="">일반 쇼파</a></li>
+                                    <li class="cate-li" v-on:click.prevent="showProductList('lounge-sofa')"><a href="">좌식 쇼파</a></li>
+                                    <li class="cate-li" v-on:click.prevent="showProductList('single-sofa')"><a href="">1인용 쇼파</a></li>
                                 </ul>
                             </ul>
                         </li>
@@ -48,8 +48,8 @@
                                     <span v-else> ▽</span> <!-- 화살표 아이콘 -->
                                 </li>
                                 <ul v-show="showCategories03"> <!-- 카테고리 리스트 -->
-                                    <li class="cate-li"><a href="">식탁</a></li>
-                                    <li class="cate-li"><a href="">사이드 테이블</a></li>
+                                    <li class="cate-li" v-on:click.prevent="showProductList('dining-table')"><a href="">식탁</a></li>
+                                    <li class="cate-li" v-on:click.prevent="showProductList('side-table')"><a href="">사이드 테이블</a></li>
                                 </ul>
                             </ul>
                         </li>
@@ -61,9 +61,9 @@
                                     <span v-else> ▽</span> <!-- 화살표 아이콘 -->
                                 </li>
                                 <ul v-show="showCategories04"> <!-- 카테고리 리스트 -->
-                                    <li class="cate-li"><a href="">옷장</a></li>
-                                    <li class="cate-li"><a href="">행거</a></li>
-                                    <li class="cate-li"><a href="">붙박이장</a></li>
+                                    <li class="cate-li" v-on:click.prevent="showProductList('closet')"><a href="">옷장</a></li>
+                                    <li class="cate-li" v-on:click.prevent="showProductList('hanger')"><a href="">행거</a></li>
+                                    <li class="cate-li" v-on:click.prevent="showProductList('built-in')"><a href="">붙박이장</a></li>
                                 </ul>
                             </ul>
                         </li>
@@ -93,7 +93,7 @@
                         </div>
                     </div>
                 </div>
-                <span class="manage-products">상품 관리 > 전체 상품 보기</span>
+                <span class="manage-products">상품 관리</span>
                 <div class="bed">
                     <h3><br>전체 상품</h3>
                     <div class="product" v-bind:key="i" v-for="(productVo, i) in productList">
@@ -103,7 +103,7 @@
                             <span class="product-name">{{ productVo.product_name }}</span>
                             <div class="review-manager">
                                 <span v-for="(star, i) in 5" :key="i" class="star">
-                                    <img v-if="3 > i" src="@/assets/images/homedeco/star.png" alt="">
+                                    <img v-if="productVo.avg_star > i" src="@/assets/images/homedeco/star.png" alt="">
                                     <img v-else src="@/assets/images/homedeco/star2.png" alt="">
                                 </span>
                             </div>
@@ -126,14 +126,14 @@
 
 <script>
 import "@/assets/css/managerY/productLIst.css"
-import AppHeader from '@/components/AppHeader.vue';
+import AppHeaderManager from '@/components/AppHeaderManager.vue';
 import AppFooter from '@/components/AppFooter.vue';
 import axios from 'axios';
 
 export default {
     name: "ProductListView",
     components: {
-        AppHeader,
+        AppHeaderManager,
         AppFooter
     },
     data() {
@@ -206,7 +206,7 @@ export default {
 
                 responseType: 'json' //수신타입
             }).then(response => {
-                console.log(response.data); //수신데이타
+                console.log(response.data.apiData); //수신데이타
                 this.productList = response.data.apiData;
 
             }).catch(error => {
@@ -222,6 +222,21 @@ export default {
             }).then(response => {
                 console.log(response.data); //수신데이타
                 this.productList = this.productList.filter(item => item.product_no !== product_no);
+            }).catch(error => {
+                console.log(error);
+            });
+        },
+        showProductList(categoty){
+            console.log("리스트: "+ categoty);
+            axios({
+                method: 'post',  //put,post,delete
+                url: `${this.$store.state.apiBaseUrl}/home/manager/categorylist`,
+                headers: { "Content-Type": "application/json; charset=utf-8" }, //전송타입
+                params: {category: categoty},
+                responseType: 'json' //수신타입
+            }).then(response => {
+                console.log(response.data.apiData); //수신데이타
+                this.productList = response.data.apiData;
             }).catch(error => {
                 console.log(error);
             });
