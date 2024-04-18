d01e90f6b191a0c369ae186432904fbf42c48ddb
aassdd236 <aassdd236@naver.com>
2024-04-18 12:29:51 +0900

    .

--- src/components/AppHeaderManager.vue
+++ src/components/AppHeaderManager.vue
@@ -1,44 +1,43 @@
 <template>
 
-<header>
-    <div class="ss-header0">
-        <div class="ss-header1">
-            <!-- 누르면 둘다 홈으로 -->
-            <img src="../assets/images/homedeco/logo.png">
-            <span><router-link to="/">집꾸며조</router-link></span>
-        </div>
-        <!-- //header1 -->
+    <header>
+        <div class="ss-header0">
+            <div class="ss-header1">
+                <!-- 누르면 둘다 홈으로 -->
+                <img src="../assets/images/homedeco/logo.png">
+                <span><router-link to="/">집꾸며조</router-link></span>
+            </div>
+            <!-- //header1 -->
 
-        <div class="ss-header4">
-            
-        </div>
-        <!-- //header2 -->
+            <div class="ss-header4">
 
-        <div class="ss-header3">
-            <img src="../assets/images/homedeco/cart.png">
-            <img v-if="this.$store.state.nowOrderList === null" id="ss-red" src="../assets/images/homedeco/red.png">
-            <!-- 로그인하기전 -->
-            <ul v-if="this.$store.state.token === null">
-                <li><router-link to="/login">로그인</router-link></li>
-                <li><a>회원가입</a></li>
-                <li><a>고객센터</a></li>
-            </ul>
-            <ul v-if="this.$store.state.token !== null && this.$store.state.authUser !== null">   
-                <li><router-link to="/mypage">ㅁㅁㅁ님</router-link></li>
-                <li><a v-on:click="logOut">로그아웃</a></li>
-                <li><router-link to="/pay">결제페이지</router-link></li>
-            </ul>
-            <ul v-if="this.$store.state.userNo === 0">   
-                <li><router-link to="/manager/productlist">관리자</router-link></li>
-                <li><a v-on:click="logOut">로그아웃</a></li>
-                <li><router-link to="/manager/productlist">관리페이지</router-link></li>
-            </ul>
+            </div>
+            <!-- //header2 -->
+
+            <div class="ss-header3">
+                <!-- 로그인하기전 -->
+                <ul v-if="this.$store.state.token === null">
+                    <li><router-link to="/login">로그인</router-link></li>
+                    <li><a>회원가입</a></li>
+                    <li><a>고객센터</a></li>
+                </ul>
+                <ul v-if="this.$store.state.token != null && this.$store.state.authUser != null">
+                    <router-link to="/cart"><img src="../assets/images/homedeco/cart.png"></router-link>
+                    <p id="headerCartCount">{{ cartCount }}</p>
+                    <li><router-link to="/mypage">ㅁㅁㅁ님</router-link></li>
+                    <li><a v-on:click="logOut">로그아웃</a></li>
+                </ul>
+                <ul v-if="this.$store.state.userNo === 0">
+                    <li><router-link to="/manager/productlist">관리자</router-link></li>
+                    <li><a v-on:click="logOut">로그아웃</a></li>
+                    <li><router-link to="/manager/productlist">관리페이지</router-link></li>
+                </ul>
+            </div>
+            <!-- //header3 -->
         </div>
-        <!-- //header3 -->
-    </div>
-    <!-- header0 -->
-</header>
-<!-- //header 부분 -->
+        <!-- header0 -->
+    </header>
+    <!-- //header 부분 -->
 
 </template>
 
@@ -46,33 +45,34 @@
 import '@/assets/css/main/ss-home.css';
 
 export default {
-    name : 'AppHeader',
-    components : {
+    name: 'AppHeader',
+    components: {
 
     },
-    data (){
+    data() {
         return {
-            keyword : ''
+            keyword: '',
+            cartCount: this.$store.state.cartCount
         }
     },
-    methods : {
-        search(){
-            this.$emit('update',this.keyword);
-            // this.$router.push('/category');
+    methods: {
+        search() {
+            this.$emit('update', this.keyword);
+            this.$router.push('/category');
+            this.keyword = '';
         },
-        logOut(){
-            this.$store.commit('setAuthUser',null);
+        logOut() {
+            this.$store.commit('setAuthUser', null);
             this.$store.commit('setToken', null);
             this.$store.commit('setNowPayment', null);
-        }
+            this.$router.push('/');
+        },
+
     },
-    created (){
-        this.keyword = '';
+    created() {
     }
 }
 
 </script>
 
-<style>
-
-</style>
+<style></style>
