8a5b64613d050169f58e239b12c03741c4488479
yss9358 <93y2s@naver.com>
2024-04-19 16:29:12 +0900

    ??

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
-<<<<<<< HEAD
   outputDir: "C://javastudy//homedeco//api_home//src//main//resources//static"
-=======
-  outputDir: "C:\\javastudy\\homedeco\\api-home\\src\\main\\resources\\static"
->>>>>>> d58fca0f1411fca448bead8ede392ec03d4488be
+
 })
